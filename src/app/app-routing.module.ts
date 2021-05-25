07877dcc25b1faabe879fa4290db54fd1360826b
Ankush-CodeHub <84678116+Ankush-CodeHub@users.noreply.github.com>
2021-05-25 20:16:20 +0530

    deployment first trail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserLoginComponent } from './user-login/user-login.component'
-import { HomepageComponent } from './homepage/homepage.component'
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
-  { path: 'user-login', component: UserLoginComponent }
+  { path: 'login', component: UserLoginComponent },
+  { path: 'register', component: UserRegisterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { UserLoginComponent } from './user-login/user-login.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLoginComponent,
+    UserRegisterComponent,
     HomepageComponent
   ],
   imports: [
